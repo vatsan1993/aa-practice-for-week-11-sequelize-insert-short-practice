5be75dea9e994806d7060461171dba98a4687418
Srivatsan <vatsan1993@gmail.com>
2024-10-21 18:03:36 +0530

    finished project

--- server/app.js
+++ server/app.js
@@ -12,11 +12,10 @@ const { Puppy } = require('./db/models');
 
 // Index of all puppies - DO NOT MODIFY
 app.get('/puppies', async (req, res, next) => {
-    const allPuppies = await Puppy.findAll({ order: [['name', 'ASC']] });
+  const allPuppies = await Puppy.findAll({ order: [['name', 'ASC']] });
 
-    res.json(allPuppies);
+  res.json(allPuppies);
 });
-
 
 // STEP 1
 // Capture the name, ageYrs, breed, weightLbs, and microchipped attributes
@@ -24,8 +23,31 @@ app.get('/puppies', async (req, res, next) => {
 // Use these values to BUILD a new Puppy in the database.
 // Respond to the request by sending a success message
 app.post('/puppies/build', async (req, res, next) => {
-    // Your code here
-})
+  // Your code here
+  let { name, ageYrs, breed, weightLbs, microchipped } = req.body;
+  try {
+    let newPuppy = Puppy.build({
+      name,
+      ageYrs,
+      breed,
+      weightLbs,
+      microchipped,
+    });
+    await newPuppy.save();
+    res.status(200).json({
+      message: 'success',
+      data: newPuppy,
+    });
+  } catch (err) {
+    let status = 500;
+    if (err.name === 'SequelizeValidationError') {
+      status = 422;
+    }
+    res.status(status).json({
+      message: err.message,
+    });
+  }
+});
 
 // STEP 2
 // Capture the name, ageYrs, breed, weightLbs, and microchipped attributes
@@ -33,21 +55,40 @@ app.post('/puppies/build', async (req, res, next) => {
 // Use these values to CREATE a new Puppy in the database.
 // Respond to the request by sending a success message
 app.post('/puppies/create', async (req, res, next) => {
-    // Your code here
-})
+  // Your code here
+  let { name, ageYrs, breed, weightLbs, microchipped } = req.body;
+  try {
+    let newPuppy = await Puppy.create({
+      name,
+      ageYrs,
+      breed,
+      weightLbs,
+      microchipped,
+    });
 
+    res.status(200).json({ message: 'success', data: newPuppy });
+  } catch (err) {
+    let status = 500;
+    if (err.name === 'SequelizeValidationError') {
+      status = 422;
+    }
+    res.status(status).json({
+      message: err.message,
+    });
+  }
+});
 
 // Root route - DO NOT MODIFY
 app.get('/', (req, res) => {
-    res.json({
-        message: "API server is running"
-    });
+  res.json({
+    message: 'API server is running',
+  });
 });
 
 // Set port and listen for incoming requests - DO NOT MODIFY
 if (require.main === module) {
-    const port = 8000;
-    app.listen(port, () => console.log('Server is listening on port', port));
+  const port = 8000;
+  app.listen(port, () => console.log('Server is listening on port', port));
 } else {
-    module.exports = app;
+  module.exports = app;
 }
--- server/script.js
+++ server/script.js
@@ -10,12 +10,18 @@ const build = async () => {
     // breed: Brittany Spaniel
     // microchipped: false
     // Your code here
-
-
+    let data = {
+      name: 'Trudy',
+      ageYrs: 2,
+      weightLbs: 38,
+      breed: 'Brittany Spaniel',
+      microchipped: false,
+    };
+    let newPuppy = Puppy.build(data);
+    await newPuppy.save();
   } catch (err) {
     throw err;
   }
-
 };
 
 const create = async () => {
@@ -30,8 +36,15 @@ const create = async () => {
     // breed: Bulldog
     // microchipped: true
     // Your code here
+    let data = {
+      name: 'Beans',
+      ageYrs: 1.6,
+      weightLbs: 42,
+      breed: 'Bulldog',
+      microchipped: true,
+    };
 
-
+    let newPuppy = await Puppy.create(data);
   } catch (err) {
     throw err;
   }
@@ -41,17 +54,17 @@ if (require.main === module) {
   require('dotenv').config();
   const { resetDB, seedAllDB } = require('./test/utils/test-utils');
   (async () => {
-    await resetDB("db/dev.db");
-    await seedAllDB("db/dev.db");
+    await resetDB('db/dev.db');
+    await seedAllDB('db/dev.db');
     try {
       await build();
-    } catch(err) {
+    } catch (err) {
       console.error('There was an error thrown while building:');
       console.error(err);
     }
     try {
       await create();
-    } catch(err) {
+    } catch (err) {
       console.error('There was an error thrown while creating:');
       console.error(err);
     }
